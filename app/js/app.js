1f9d26d8e4b430433eadfb21ca81dcd2f31f614b
OlgaKomar <likagt17.17@gmail.com>
2015-04-21 23:14:48 +0300

    Add calculation handler to Keys controller

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,56 @@
         $scope.clerScreen = function(){
             $scope.inputText = "";
         };
+        $scope.transformTextToNumberArr = function (text) {
+            var val = text,
+                arrValues = [];
+            while(val){
+              arrValues.push(parseFloat(val));
+              val = val.slice(arrValues[arrValues.length-1].toString().length);
+              if(val.length == 1) break;
+              if (val) {
+                arrValues.push(val[0]);
+                val = val.slice(1);
+              }
+            }
+            return arrValues;
+        };
+        $scope.analyzeOperatorsInArr = function (arrValues) {
+            for (var i = 0; i < arrValues.length; i++) {
+
+              if (arrValues[i] == "x" || arrValues[i] == "*" || arrValues[i] == "/") {
+                (arrValues[i] == "/") ? arrValues[i-1] /= arrValues[i+1] : arrValues[i-1] *= arrValues[i+1];
+                arrValues.splice(i, 2);
+                i--;
+                continue;
+              }
+              if (arrValues[i] == "-") {
+                arrValues[i+1] = parseFloat("-" + arrValues[i+1]);
+                arrValues.splice(i, 1);
+              }
+              if (arrValues[i] == "+" || arrValues[i] == ".") {
+                arrValues.splice(i, 1);
+              }
+
+                console.log(arrValues);
+            };
+            return arrValues;
+        };
+        $scope.executeCalc = function(){
+            var arrValues = $scope.transformTextToNumberArr($scope.inputText);
+                arrValues = $scope.analyzeOperatorsInArr(arrValues),
+                sum = 0;
+            for (var i = 0, l = arrValues.length; i < l; i++) {
+              if (!(typeof arrValues[i] === "number") || isNaN(arrValues[i])) {
+                sum = "NaN";
+                break;
+              }
+              sum += arrValues[i];
+            }
+            arrValues = [];
+            return (!(typeof sum === "number") || isNaN(sum)) ? 
+                $scope.inputText = "NaN" : $scope.inputText = sum;
+        }
     })
     .directive('calculatorDisplay', function(){
         return {
